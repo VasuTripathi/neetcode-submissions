b75d18616ee8345a91ce04bd918d2e5d3a9e897c
Vasu Tripathi <110618054+VasuTripathi@users.noreply.github.com>
2026-05-21 14:14:40 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        char[] ch = s.toCharArray();
+        for (int i = 0; i < s.length(); i++) {
+            if (ch[i] == '(' || ch[i] == '[' || ch[i] == '{') {
+                stack.push(ch[i]);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if ((ch[i] == ')' && top != '(') || (ch[i] == ']' && top != '[')
+                    || (ch[i] == '}' && top != '{')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
